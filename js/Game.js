7b3421b83f814bc87292f73853a622f9e873e9dd
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-04-28 15:35:14 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,18 @@
+class Game {
+  constructor() {}
+  
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+ 
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+ 
+}
